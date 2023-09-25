df58e833feebc323bc978f8a0eecfba780568716
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-25 14:15:28 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,34 @@
+// Here let, var, const are varibles 
+
+let name = 'Sudhakar';
+console.log(name)
+
+let firstName = 'SudhakarReddy';
+let lastName ='Reddy';
+console.log(firstName);
+console.log(lastName);
+
+
+let subject1 = 'Python';
+let subject2 = 'javascript';
+let subject3 = 'Django';
+console.log(subject1);
+console.log(subject2);
+console.log(subject3);
+
+var studentName = 'Sudhakar';
+var studentRoll = '431';
+var studentlocation = 'Hyderabad'
+
+console.log(studentName);
+console.log(studentRoll);
+console.log(studentlocation);
+
+
+const employee = 'SadieSink';
+const empoyeeDept = 'Python';
+const employeeLocation = "USA";
+
+console.log(employee)
+console.log(empoyeeDept)
+console.log(employeeLocation)
