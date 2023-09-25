7ea55eebe5135b154a7d7c0c5856e933cfe4e1ea
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-25 14:15:15 +0530

    Datatypes

---
+++ primitivedatatypes.js
@@ -0,0 +1,18 @@
+let name ='sudhakar';
+let age = 20;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(name);
+console.log(typeof(name));
+console.log(name.length);
+
+console.log(age);
+console.log(typeof(age));
+console.log(isApproved);
+console.log(typeof(isApproved));
+console.log(firstName);
+console.log(typeof(firstName));
+console.log(selectedColor);
+console.log(typeof(selectedColor));
