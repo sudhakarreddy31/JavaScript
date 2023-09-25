b04620154f0b4fc750ce5c9b2bfe51790ae63c8b
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-25 14:14:44 +0530

    Arrays

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let selectedColor=['red','blue','orange'];
+console.log(selectedColor)
+console.log(selectedColor.length)
+
+
+let colors=['bule','pink'];
+colors[2]='green'           //Add anther array
+console.log(colors)
+console.log(colors.length)
+
+let sports=['cricket','hokey'];
+sports[2]='kabbadi';
+sports[3]='ruby';
+console.log(sports)
+console.log(sports.length)
+
+
+
