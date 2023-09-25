223a14d497377889e63e4c0e682296e303345b89
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-09-25 14:14:58 +0530

    function

---
+++ function.js
@@ -0,0 +1,62 @@
+function greet(){
+
+    console.log('Helloo');
+}
+
+greet();
+
+
+function chinnna(){
+    console.log('HelooMoto..')
+}
+
+chinnna();
+
+function foo(name){
+    console.log(name + 'hello')
+}
+
+foo('chinna')
+
+
+function alpha(name,lastName){
+    console.log('Hello' + ''+name + ''+lastName)
+}
+alpha('msr')
+alpha('reddy')
+
+
+
+function square(number){
+    return number * number;
+
+}
+
+let number = square(2)
+console.log(number)
+// console.log(square(2))
+
+
+function add(num){
+
+    return num + num
+}
+let num = add(8);
+console.log(add(8))
+
+function sub(subb){
+    return subb - subb
+
+
+}
+
+let subb = sub(5);
+console.log(sub(5));
+
+
+function division(div){
+    return div % div 
+}
+
+let div = division(8)
+console.log(division(8))
